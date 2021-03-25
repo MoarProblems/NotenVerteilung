0b004387e503554e7f64535fcf3bd9a6e98eed52
MoarProblems <modderscraft@gmail.com>
2021-03-25 19:20:18 +0100

    Create dbCreationScript.sql

---
+++ dbCreationScript.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE NotenDB;
+USE NotenDB;
+
+CREATE TABLE GET (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE GP (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE InfI (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE MatI (grade float CHECK (grade BETWEEN 1 AND 6));
