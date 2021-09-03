5e3f0430abae32eaa19da9bbea6b8e22f027ee25
MoarProblems <modderscraft@gmail.com>
2021-09-03 19:57:41 +0200

    Updated for second semester

--- dbCreationScript.sql
+++ dbCreationScript.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE NotenDB;
 USE NotenDB;
 
+--Semester 1
 CREATE TABLE GET (grade float CHECK (grade BETWEEN 1 AND 6));
 
 CREATE TABLE GP (grade float CHECK (grade BETWEEN 1 AND 6));
@@ -8,3 +9,16 @@ CREATE TABLE GP (grade float CHECK (grade BETWEEN 1 AND 6));
 CREATE TABLE InfI (grade float CHECK (grade BETWEEN 1 AND 6));
 
 CREATE TABLE MatI (grade float CHECK (grade BETWEEN 1 AND 6));
+
+--Semester 2
+CREATE TABLE MatII (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE GETII (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE INFII (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE Phys (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE Digi (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE EMI (grade float CHECK (grade BETWEEN 1 AND 6));
