7c608e78768243a4b6c0d5a0b157a12b5ca4fc3a
MoarProblems <modderscraft@gmail.com>
2022-01-10 14:24:37 +0100

    Updated for third semester

--- dbCreationScript.sql
+++ dbCreationScript.sql
@@ -22,3 +22,12 @@ CREATE TABLE Phys (grade float CHECK (grade BETWEEN 1 AND 6));
 CREATE TABLE Digi (grade float CHECK (grade BETWEEN 1 AND 6));
 
 CREATE TABLE EMI (grade float CHECK (grade BETWEEN 1 AND 6));
+
+--Semester 3
+CREATE TABLE MatIII (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE GETIII (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE SYS (grade float CHECK (grade BETWEEN 1 AND 6));
+
+CREATE TABLE EBUS (grade float CHECK (grade BETWEEN 1 AND 6));
